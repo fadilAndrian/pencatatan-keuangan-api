9831480216272a759482cec43c8d68cd0a9976ff
fadilAndrian <akhsanfadil@gmail.com>
2021-09-09 08:43:14 +0700

    transactions table created

--- database/migrations/2021_09_09_013605_create_transactions_table.php
+++ database/migrations/2021_09_09_013605_create_transactions_table.php
@@ -15,6 +15,11 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->String("transaksi");
+            $table->Integer("jumlah");
+            $table->date("tanggal");
             $table->timestamps();
         });
     }
